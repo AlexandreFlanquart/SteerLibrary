6a507f2aba43c4c9b1a58c1384f1148ddab59def
AlexandreFlanquart <alexflanqu@gmail.com>
2023-01-30 08:44:18 +0100

    :tada: Initial commit

---
+++ Source/SteerLibrary/SteerLibraryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteerLibraryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEERLIBRARY_API ASteerLibraryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
